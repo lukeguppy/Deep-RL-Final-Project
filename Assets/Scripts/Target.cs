05c185a537a91965efec209af596ebd079bd8df1
Luke <lukewguppy@gmail.com>
2024-04-21 21:49:06 +0100

    Commented target script

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Class for defining target points in the environment
 public class Target : MonoBehaviour
 {
+    // Public variables for coordinates and state of the target point
     public float x;
     public float y;
     public float z;
@@ -15,21 +17,26 @@ public class Target : MonoBehaviour
     public int waiting = 0;
     public int carLimit = 2;
 
+    // Start is called before the first frame update
     void Start()
     {
+        // Set coordinates from transform position
         SetCoordinatesFromTransform();
     }
 
+    // Method for force starting target
     public void ForceStart()
     {
         Start();
     }
 
-    public Vector3 GetCoords() 
-    {  
+    // Method for getting coordinates of the target
+    public Vector3 GetCoords()
+    {
         return new Vector3(x, y, z);
     }
 
+    // Method for setting coordinates from transform position
     public void SetCoordinatesFromTransform()
     {
         x = transform.position.x;
